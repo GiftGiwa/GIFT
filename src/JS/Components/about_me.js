403aacc552aa08d95af40345f4727610824889aa
GiftGiwa <gdg2131@columbia.edu>
2022-08-13 14:37:45 -0400

    .

--- src/JS/Components/about_me.js
+++ src/JS/Components/about_me.js
@@ -7,11 +7,11 @@ function AboutMe () {
         <div id="aboutMe">
             <h3 className="headings">.about-me</h3>
             <div id="portrait"></div>
-            <p class="main-text delay" id="_1">I’m currently a sophomore Computer Science student at Columbia University. I’ve worked with these technologies so far:</p>
-            <br></br>
 
-            <p class="main-text delay" id="_2">I’m interested in app/game development (mainly front-end), and computer graphics. I also like playing video games (especially Mario Kart) and – though I’m new to them – music production and creating 3D designs. 
-            </p>
+            <p className="main-text body delay" id="_1">I’m currently a sophomore Computer Science student at Columbia University. I’ve worked with these technologies so far:</p>
+            <br></br>
+            <p className="main-text body delay" id="_2">I’m interested in app/game development (mainly front-end), and computer graphics. I also like playing video games (especially Mario Kart) and – though I’m new to them – music production and creating 3D designs.</p>
+
         </div>
     )
 }
