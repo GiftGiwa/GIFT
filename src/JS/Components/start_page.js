96ea59364b6474af864be104ffe6dbd6bccf205b
GiftGiwa <gdg2131@columbia.edu>
2022-08-13 00:38:30 -0400

    .

--- src/JS/Components/start_page.js
+++ src/JS/Components/start_page.js
@@ -8,26 +8,60 @@ function StartPage() {
 
     function slide() {
 
-        function move_up(id) {
+        anime({
+            targets: '#moving',
+            easing: "linear",
+            translateX: -2520,
+            translateY: -80,
+            duration: 0,
+        });
+
+        function move_up() {
+            // document.getElementById("bg_rect_2").style.top = "0px"
             anime({
-                targets: id,
+                targets: '#bg_rect_2',
                 translateY: -1 * window.innerHeight,
                 easing: "easeOutExpo",
                 duration: 1500
             })
+
+            anime({
+                targets: '#moving',
+                easing: 'easeOutSine',
+                loop: true,
+                translateX: [
+                    {duration: 1500, value: '+=80px',},
+                    {duration: 1500, value: '+=0px',},
+                    {duration: 1500, value: '-=80px',},
+                    {duration: 1500, value: '+=0px',
+                    },
+                  ],
+                  translateY: [
+                    {duration: 1500, value: '+=0px',},
+                    {duration: 1500, value: '+=80px',},
+                    {duration: 1500, value: '+=0px',},
+                    {duration: 1500, value: '-=80px',},
+                  ],
+                duration: 3000
+            });
+
             setTimeout(function () {
+                //document.getElementById("bg_rect_2").style.top = "0px"
                 document.getElementById("bg_rect").style.top = "0px"
                 document.body.height = 4315
+
                 document.getElementById("html").height = 4315
                 document.getElementById("html").style.overflowY = "scroll"
                 document.getElementById("html").style.overflowX = "hidden"
             }, 1500)
+
         }
-        move_up('#bg_rect')
+
+        move_up()
        
         // anime({
         //     targets: '#moving',
-        //     easing: 'easeInOutSine',
+        //     easing: 'easeOutSine',
         //     loop: true,
         //     translateX: [
         //         {duration: 1500, value: '+=80px',},
@@ -45,13 +79,7 @@ function StartPage() {
         //     duration: 3000
         // });
 
-        anime({
-            targets: '#moving',
-            easing: "linear",
-            translateY: 80,
-            loop:true,
-            duration: 2000,
-        });
+        
     }
 
 
@@ -71,10 +99,14 @@ function StartPage() {
 
             <div id="bg_rect"></div>
 
-            <div id="bg_rect_2"></div>
+            <div id="bg_rect_2">
+                <Grid id = "static" />
+                <Grid id = "moving" />
+    
+            </div>
 
-            <Grid id = "static" />
-            <Grid id = "moving" />
+            {/* <Grid id = "static" />
+            <Grid id = "moving" /> */}
     
         </div>   
     )
