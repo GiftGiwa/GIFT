eb7726c9efff35041997e73d5a22cf6b1d0fcb06
GiftGiwa <gdg2131@columbia.edu>
2022-08-23 16:48:33 -0400

    .

--- src/JS/Components/experience.js
+++ src/JS/Components/experience.js
@@ -5,6 +5,17 @@ import ExperienceCard from "./experience_card"
 
 function Experience() {
 
+    const experience = [ // **to be filled over time
+        {
+            count: "01",
+            company: "Columbia Build Lab",
+            position: "SWE Intern",
+            firstBullet: "Development of Sharp Performance, an Android/iOS compatible application that provides a plethora of cognitive performance exercises to the user to maintain mental fortitude while transitioning between high- and low-stress environments, based on methods delivered to U.S. Military Special Operations personnel.",
+            secondBullet: "Improved login time by 4+ seconds on average in the back-end login system using C# and MySQL in Unity.",
+            thirdBullet: "Increased potential user base by 300% by app deployment to iOS devices",
+        }
+    ]
+
     return (
         <div className="horizontal-center" id = "experience">
             
@@ -12,7 +23,7 @@ function Experience() {
                 <h3 className="headings">.experience</h3>
             </div>
 
-            <ExperienceCard color="#45D4B2" id="build-lab" count="01"></ExperienceCard>
+            <ExperienceCard color="#45D4B2" id="build-lab" count={experience[0].count} company={experience[0].company} position={experience[0].position}></ExperienceCard>
         </div>
     )
 }
--- src/JS/Components/experience_card.js
+++ src/JS/Components/experience_card.js
@@ -1,9 +1,12 @@
 import React from "react"
+import "../../CSS/components.css"
 import "../../CSS/experience_card.css"
 
+
 function ExperienceCard(props) {
+
     return (
-        <div style={{color: props.color}}id="experience-card">
+        <div style={{borderColor: props.color}} className="body" id="experience-card">
             <h4 style={{color: props.color}}>{props.count}</h4>
             <p style={{color:props.companyColor}}>{props.company}, {props.position}</p>
             <li>{props.firstBullet}</li>
