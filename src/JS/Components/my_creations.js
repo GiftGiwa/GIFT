db5d97f70988f63b55c8443c21085ccec081fcc9
GiftGiwa <gdg2131@columbia.edu>
2022-09-04 12:39:39 -0400

    .

--- src/JS/Components/my_creations.js
+++ src/JS/Components/my_creations.js
@@ -1,6 +1,7 @@
 import "../../CSS/my_creations.css"
 import "../../CSS/components.css"
-
+import sudoku from "../../Images/sudoku.png"
+import personal_site from "../../Images/personal_site.png"
 
 function MyCreations() {
     return (
@@ -10,7 +11,8 @@ function MyCreations() {
             </div>
 
             <div id = "projects">
-                <img id = "sudoku" src="../../../public/Images/sudoku.png"></img>
+                <img id = "sudoku" src={sudoku}></img>
+                <img id = "personal_site" src={personal_site}></img>
             </div>
         </div>
     )
