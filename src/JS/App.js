b083bf55d3f99621472dde6d190bb87e38280bf4
GiftGiwa <gdg2131@columbia.edu>
2022-09-10 20:39:26 -0400

    .

--- src/JS/App.js
+++ src/JS/App.js
@@ -4,14 +4,14 @@ import AboutMe from "./Components/about_me"
 import Credits from "./Components/credits"
 import Experience from "./Components/experience"
 import MyCreations from "./Components/my_creations"
+import Navbar from "./Components/navbar"
 import DecoGrid from "./Components/grid"
 
 function App() {
   
   return (
     <>
-
-    
+      <Navbar></Navbar>
       <Page />
       <AboutMe />
       <Experience />
--- src/JS/Components/navbar.js
+++ src/JS/Components/navbar.js
@@ -1,13 +1,14 @@
 import "../../CSS/navbar.css"
+import "../../CSS/components.css"
 
 function Navbar() {
     return (
         <div id = "navbar">
             <ul>
                 <li><a href="default.asp">Home</a></li>
-                <li><a href="news.asp">News</a></li>
-                <li><a href="contact.asp">Contact</a></li>
-                <li><a href="about.asp">About</a></li>
+                <li><a href="news.asp">.about-me</a></li>
+                <li><a href="contact.asp">.experience</a></li>
+                <li><a href="about.asp">.my-creations</a></li>
             </ul>
         </div>
     )
