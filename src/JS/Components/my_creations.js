22a8563d0b8fd7ef9f81d13fcc849888f8ad4c1e
GiftGiwa <gdg2131@columbia.edu>
2022-09-14 12:51:39 -0400

    .

--- src/JS/Components/my_creations.js
+++ src/JS/Components/my_creations.js
@@ -1,20 +1,19 @@
 import "../../CSS/my_creations.css"
 import "../../CSS/components.css"
-import "../../CSS/descriptions.css"
 import sudoku from "../../Images/sudoku.png"
 import personal_site from "../../Images/personal_site.png"
 import Filler from "./filler"
+import Project from "./project"
 
 function MyCreations() {
     
     setTimeout(function () {
-
         //apply query selector for multiple divs instead of getElementById !
         let sudoku = document.getElementById('sudoku_desc')
         const onMouseMove = (e) => {
+
             let bounds = e.target.getBoundingClientRect();
             //console.log(bounds.left, bounds.top)
-
             if (bounds.left != undefined && bounds.top != undefined && bounds.top != 0 && bounds.left != 0) {
                 sudoku.style.left = (e.clientX - bounds.left + 30) + "px";
                 sudoku.style.top = (e.clientY - bounds.top + 30) + "px";
@@ -35,16 +34,18 @@ function MyCreations() {
             <div id = "projects-body">
 
                 <div id = "projects">
-                    <div className = "project" id = "sudoku"><img className = "images" src={sudoku}></img></div>
+                    {/* <div className = "project" id = "sudoku"><img className = "images" src={sudoku}></img></div>
                     <div className = "desc body" id = "sudoku_desc">
                         <p className = "body">Sudoku
                         <br></br> 
                         
                         Sudoku puzzle generator/solver, built in vanilla HTML/CSS/JavaScript.
                         </p>
-                    </div>
+                    </div> */}
+                    <Project project = "sudoku" imageSource={sudoku}></Project>
+                    <Project project = "personalSite" imageSource={personal_site}></Project>
 
-                    <div className = "project" id = "personal_site"><img className = "images" src={personal_site}></img></div>
+                    {/* <div className = "project" id = "personal_site"><img className = "images" src={personal_site}></img></div> */}
                     <Filler bgcolor="rgba(255, 255, 255, 0.06)" /><Filler bgcolor="rgba(255, 255, 255, 0.04)"/><Filler bgcolor="rgba(255, 255, 255, 0.025)"/>
                 </div>
 
---
+++ src/JS/Components/project.js
@@ -0,0 +1,23 @@
+import "../../CSS/project.css"
+import "../../CSS/components.css"
+import sudoku from "../../Images/sudoku.png"
+import personal_site from "../../Images/personal_site.png"
+
+function Project(props) {
+
+    return (
+        <>
+            <div className = "project" id = {props.project} ><img className = "images" src={props.imageSource}></img></div>
+            <div className = "desc" id = "sudoku_desc">
+                <p className = "body">Test</p>
+            </div>
+        </>       
+    )
+}
+
+export default Project
+
+// Sudoku
+//                 <br></br> 
+                
+//                 Sudoku puzzle generator/solver, built in vanilla HTML/CSS/JavaScript.
