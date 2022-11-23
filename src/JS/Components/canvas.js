541efb0719d24ded466dc4e8db5f1c142be6839b
GiftGiwa <gdg2131@columbia.edu>
2022-11-23 11:54:40 -0500

    Update canvas.js

--- src/JS/Components/canvas.js
+++ src/JS/Components/canvas.js
@@ -1,15 +1,16 @@
-import React, { Component }  from 'react';
-//import three from "./sphere_background/sphere_animation.js"
+import React, { Component } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { RGBELoader } from 'sphere_background/imports/RGBELoader.js';
 
+class Spheres extends Component() {
+    componentDidMount() {
 
-function Canvas() {
+    }
 
-    //three()
-
-    return (
-        // <div id = "canvas"></div>
-        <canvas></canvas>
-    )
+    render() {
+        return <div ref={ref => (this.mount = ref)} />;
+    }
 }
 
 export default Canvas
