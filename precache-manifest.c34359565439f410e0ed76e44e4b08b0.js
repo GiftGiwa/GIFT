9a048abca2231dc2bdd709fa68c5e4216ff989d7
GiftGiwa <gdg2131@columbia.edu>
2022-11-27 01:40:07 -0500

    Updates

---
+++ precache-manifest.c34359565439f410e0ed76e44e4b08b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f21de14fa82396629b9c",
+    "url": "/gift-giwa/static/css/main.7048879e.chunk.css"
+  },
+  {
+    "revision": "f21de14fa82396629b9c",
+    "url": "/gift-giwa/static/js/main.8b647833.chunk.js"
+  },
+  {
+    "revision": "8f583cb15e20c8d76bc0",
+    "url": "/gift-giwa/static/js/runtime~main.b72f8559.js"
+  },
+  {
+    "revision": "5d634f5467868e0e4bcc",
+    "url": "/gift-giwa/static/js/2.7eea9955.chunk.js"
+  },
+  {
+    "revision": "319229a88ac4ee485578",
+    "url": "/gift-giwa/static/js/3.2b500963.chunk.js"
+  },
+  {
+    "revision": "39a50c9cc9c6bd056f3c37e300115947",
+    "url": "/gift-giwa/static/media/sudoku.39a50c9c.png"
+  },
+  {
+    "revision": "fd564b3e0e769b46ebc0be195e04fb54",
+    "url": "/gift-giwa/static/media/personal_site.fd564b3e.png"
+  },
+  {
+    "revision": "483ef11cd7238b5cafcb446605f577b1",
+    "url": "/gift-giwa/static/media/Logos.483ef11c.svg"
+  },
+  {
+    "revision": "492a2fb90816e8cbf56a794144802c6f",
+    "url": "/gift-giwa/index.html"
+  }
+];
